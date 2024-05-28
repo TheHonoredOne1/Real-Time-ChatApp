d15c3a160f40a68756bdd0308689e7d925ea43d1
Sushrut Bawanthade <thehonoredone1@gmail.com>
2024-05-29 01:43:40 +0530

    backend + frontend till logout

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createContext, useContext, useState } from "react";
+import { useEffect } from "react";
+export const AuthContext = createContext();
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuthContext = () => {
+    return useContext(AuthContext);
+};
+
+export const AuthContextProvider = ({ children }) => {
+
+    const [authUser, setAuthUser] = useState(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("chat-user");
+        if (storedUser) {
+            try {
+                const parsedUser = JSON.parse(storedUser);
+                setAuthUser(parsedUser);
+            } catch (error) {
+                console.error('Failed to parse user from localStorage:', error);
+                setAuthUser(null);
+            }
+        }
+    }, []);
+
+
+    return (<AuthContext.Provider value={{ authUser, setAuthUser }}>
+        {children}
+    </AuthContext.Provider>);
+};
