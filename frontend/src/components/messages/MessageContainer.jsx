d99fc750f97895d731c894c2c796c25aebaeba74
Sushrut Bawanthade <thehonoredone1@gmail.com>
2024-06-01 01:31:12 +0530

    completed till getting real conversations on the frontend

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
-
-import {TiMessages} from 'react-icons/ti'
-
+import { TiMessages } from 'react-icons/ti'
+import useConversation from '../../zustand/useConversation'
 
 
 function MessageContainer() {
-    
-    const noChatSelected = true;
+
+    const { selectedConversation, setSelectedConversation } = useConversation()
+
+    useEffect(() => {
+        // cleanup function (unmounts)
+        return () => setSelectedConversation(null)
+    }, [])
 
     return (
         <div className='md:min-w-[450px] flex flex-col'>
-            {noChatSelected ? (
+            {!selectedConversation ? (
                 <NoChatSelected />
             ) : (
                 <>
                     <div className='bg-slate-500 px-4 py-2 mb-2'>
                         <span className='label-text font-semibold'>To:</span>{" "}
-                        <span className='text-gray-900 font-bold'>lol</span>
+                        <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
                     </div>
                     <Messages />
                     <MessageInput />
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversations from '../../hooks/useGetConversations'
+import { getRandomEmoji } from '../../utils/emojis';
+
 function Conversations() {
+
+    const { loading, conversations } = useGetConversations();
+    console.log("conversations : ", conversations)
+
     return (
         <div className='py-2 flex flex-col overflow-auto'>
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {
+                conversations.map((conversation, index) => (
+                    <Conversation 
+                    key={conversation._id} 
+                    conversation = {conversation}
+                    emoji = {getRandomEmoji()} 
+                    lastIdx = {index === conversations.length - 1}
+                    />
+                ))
+            }
         </div>
     )
 }
